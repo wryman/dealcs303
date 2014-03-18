229f809f8cce149145deab1fb7759a91543531fa
wryman <william.ryman@utexas.edu>
2014-03-18 14:50:23 -0500

    Working but not printing in columns

--- Deal.py
+++ Deal.py
@@ -23,10 +23,22 @@ def main():
 		prize_door=random.randint(1,3)
 		guess=random.randint(1,3)
 		view=1
+		newguess=1
 		while (view==prize_door or view==guess):
 			view=random.randint(1,3)
-		print("pd",prize_door)
-		print("g",guess)
-		print("v",view)
+		while (newguess==guess or newguess==view):
+			newguess=random.randint(1,3)
+		if prize_door==newguess:
+			switch_win+=1
+		switch_probability=switch_win/times
+		no_switch_probability=1-switch_probability
+		print("Prize",  "Guess",  "View",  "New Guess")
+		print("prizedoor=",prize_door)
+		print("guess=",guess)
+		print("view=",view)
+		print("new guess=",newguess)
+		print("switch win=",switch_win)
+		print("Probability of winning if you switch=", round(switch_probability,2))
+		print("Probability of winning if you do not switch=", round(no_switch_probability,2))
 		i+=1
 main()
