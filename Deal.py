6ac69ed689f7a1cda9bda092d1b09573e3607d08
wryman <william.ryman@utexas.edu>
2014-03-18 15:37:07 -0500

    Complete with comments, final draft

--- Deal.py
+++ Deal.py
@@ -15,11 +15,14 @@
 # Date Last Modified:
 
 def main():
+	#import random functions and initialize variables
 	import random
 	times=int(input("Enter number of times you want to play:"))
 	switch_win=0
 	i=0
+	#print headers for columns using the r.just (right justified command)
 	print(str("Prize").rjust(2),str("Guess").rjust(4),str("View").rjust(6),str("New Guess").rjust(8))
+	#initialize loop to play game and switch answer
 	while (i<times):
 		prize_door=random.randint(1,3)
 		guess=random.randint(1,3)
@@ -31,6 +34,7 @@ def main():
 			newguess=random.randint(1,3)
 		if prize_door==newguess:
 			switch_win+=1
+			#calculate win probability
 		switch_probability=switch_win/times
 		no_switch_probability=1-switch_probability
 		i+=1
